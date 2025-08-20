a2bd28a8433c80893bcd13d25a8995c2c0a5d6ac
SwathantraPulicherla <swathantra.pulicherla@requisimus.com>
2025-08-20 10:42:32 +0000

    Add multiplication function and test

--- tests/test_calculator.c
+++ tests/test_calculator.c
@@ -1,11 +1,8 @@
 #include "unity.h"
-#include "calculator.h" // Add this line
+#include "calculator.h"
 
-void setUp(void) {
-}
-
-void tearDown(void) {
-}
+void setUp(void) {}
+void tearDown(void) {}
 
 void test_addition(void) {
     TEST_ASSERT_EQUAL(5, add(2, 3));
@@ -15,9 +12,14 @@ void test_subtraction(void) {
     TEST_ASSERT_EQUAL(1, subtract(3, 2));
 }
 
+void test_multiplication(void) { // <-- Add this test
+    TEST_ASSERT_EQUAL(6, multiply(2, 3));
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_addition);
     RUN_TEST(test_subtraction);
+    RUN_TEST(test_multiplication); // <-- Add this line
     return UNITY_END();
 }
