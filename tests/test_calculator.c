5a89369a4cd999c15a8de5fb5635ed36d226dd07
SwathantraPulicherla <swathantra.pulicherla@requisimus.com>
2025-08-29 07:20:34 +0000

    added new test run

--- tests/test_calculator.c
+++ tests/test_calculator.c
@@ -25,7 +25,8 @@ int main(void) {
     RUN_TEST(test_addition);
     RUN_TEST(test_subtraction);
     RUN_TEST(test_multiplication);
-    RUN_TEST(test_division); 
+    RUN_TEST(test_division);
+    RUN_TEST(test_modulo); 
     return UNITY_END();
 }
 
